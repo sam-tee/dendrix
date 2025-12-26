0c86790b6ead6f07ce697afbc758c46746731125
sam-tee <93236986+akhlus@users.noreply.github.com>
2025-12-26 12:53:49 +0000

    moved darwin flake parts

--- darwin-fp.nix
+++ darwin-fp.nix
@@ -2,13 +2,11 @@
   flake-parts-lib,
   lib,
   ...
-}: let
-  inherit (lib) mkOption types;
-in {
+}: {
   options = {
     flake = flake-parts-lib.mkSubmoduleOptions {
-      darwinConfigurations = mkOption {
-        type = types.lazyAttrsOf types.raw;
+      darwinConfigurations = lib.mkOption {
+        type = lib.types.lazyAttrsOf lib.types.raw;
         default = {};
         description = "Instantiated Darwin configurations";
       };
--- parts/flake-parts.nix
+++ parts/flake-parts.nix
@@ -1,4 +1,9 @@
-{inputs, ...}: {
+{
+  flake-parts-lib,
+  inputs,
+  lib,
+  ...
+}: {
   systems = [
     "x86_64-linux"
     "aarch64-linux"
@@ -8,4 +13,14 @@
     inputs.flake-parts.flakeModules.modules
     inputs.home-manager.flakeModules.home-manager
   ];
+
+  options = {
+    flake = flake-parts-lib.mkSubmoduleOptions {
+      darwinConfigurations = lib.mkOption {
+        type = lib.types.lazyAttrsOf lib.types.raw;
+        default = {};
+        description = "Instantiated Darwin configurations";
+      };
+    };
+  };
 }
