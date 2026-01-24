80b3b99a529b60f42477153860e721ff06581c9e
sam-tee <93236986+sam-tee@users.noreply.github.com>
2026-01-24 16:05:11 +0000

    feat: added device ids to syncthing

--- parts/homelab/syncthing.nix
+++ parts/homelab/syncthing.nix
@@ -10,7 +10,13 @@
         inherit user group;
         configDir = "/var/lib/media/syncthing";
         dataDir = "/var/lib/media";
+        guiAddress = "0.0.0.0";
+        guiPasswordFile = config.sops.secrets."syncPwd";
         settings = {
+          devices = {
+            u410 = {id = "233WQCM-C6ZS2DI-ZFZK33B-XNXG2HQ-7WCPO7A-KL7HV52-WA4QRGX-Z3ALBA6";};
+            s340 = {id = "QA2SIUE-GMC6AD7-OAEPXEU-RQZUYU6-7DHIXGJ-GMFNXKQ-QJUVRNO-N6EYRAL";};
+          };
         };
       };
       networking.firewall.allowedTCPPorts = [8384 22000];
