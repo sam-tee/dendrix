844594028d8c4dd36bd6009d2892b94196c14397
sam-tee <93236986+sam-tee@users.noreply.github.com>
2026-01-25 22:37:47 +0000

    refactor: forgejo refactor

--- parts/homelab/forgejo.nix
+++ parts/homelab/forgejo.nix
@@ -14,38 +14,36 @@
       "forgejo/adminPwd".owner = cfg.user;
       "forgejo/databasePwd".owner = cfg.database.user;
     };
-    services = {
-      forgejo = {
-        enable = true;
-        stateDir = "/var/lib/media/git";
-        lfs.enable = true;
-        database = {
-          type = "postgres";
-          passwordFile = config.sops.secrets."forgejo/databasePwd".path;
+    services.forgejo = {
+      enable = true;
+      stateDir = "/var/lib/media/git";
+      lfs.enable = true;
+      database = {
+        type = "postgres";
+        passwordFile = config.sops.secrets."forgejo/databasePwd".path;
+      };
+      settings = {
+        mailer = {
+          ENABLED = true;
+          FROM = hl.email.from;
+          PROTOCOL = "sendmail";
+          SENDMAIL_PATH = "/run/wrappers/bin/sendmail";
         };
-        settings = {
-          log.LEVEL = "Trace";
-          mailer = {
-            ENABLED = true;
-            FROM = hl.email.from;
-            PROTOCOL = "sendmail";
-            SENDMAIL_PATH = "/run/wrappers/bin/sendmail";
-          };
-          server = {
-            DOMAIN = domain;
-            ROOT_URL = "https://${domain}/";
-            HTTP_ADDR = "0.0.0.0";
-            HTTP_PORT = 3000;
-            LANDING_PAGE = "/sam-tee";
-            SSH_PORT = sshPort;
-            SSH_DOMAIN = "git-ssh.${hl.domain}";
-          };
-          service = {
-            DISABLE_REGISTRATION = true;
-            ENABLE_NOTIFY_MAIL = true;
-            REGISTER_EMAIL_CONFIRM = true;
-          };
+        server = {
+          DOMAIN = domain;
+          ROOT_URL = "https://${domain}/";
+          HTTP_ADDR = "0.0.0.0";
+          HTTP_PORT = 3000;
+          LANDING_PAGE = "/sam-tee";
+          SSH_PORT = sshPort;
+          SSH_DOMAIN = "git-ssh.${hl.domain}";
         };
+        service = {
+          DISABLE_REGISTRATION = true;
+          ENABLE_NOTIFY_MAIL = true;
+          REGISTER_EMAIL_CONFIRM = true;
+        };
+        log.LEVEL = "Trace";
       };
     };
     systemd.services.forgejo.preStart = let
