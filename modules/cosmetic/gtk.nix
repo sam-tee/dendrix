2177aae989c145cfb10cfd7f1ebdbe549d99e797
sam-tee <93236986+sam-tee@users.noreply.github.com>
2026-02-01 19:23:21 +0000

    feat: added gtk config module

---
+++ modules/cosmetic/gtk.nix
@@ -0,0 +1,19 @@
+{
+  flake.modules.homeManager.gtk = {pkgs, ...}: {
+    gtk = {
+      enable = true;
+      theme = {
+        package = pkgs.flat-remix-gtk;
+        name = "Flat-Remix-GTK-Grey-Darkest";
+      };
+      iconTheme = {
+        package = pkgs.adwaita-icon-theme;
+        name = "Adwaita";
+      };
+      font = {
+        name = "Sans";
+        size = 11;
+      };
+    };
+  };
+}
