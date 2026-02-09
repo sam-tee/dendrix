3abb039c325bfbc2b2523e083c99d7676f2fc8ee
sam-tee <93236986+sam-tee@users.noreply.github.com>
2026-02-09 15:19:36 +0000

    refactor: moved mediaUser into user config

--- modules/hosts/hosts.nix
+++ modules/hosts/hosts.nix
@@ -66,7 +66,6 @@ in {
           immich
           jellyfin
           mealie
-          navidrome
           syncthing
           tandoor
           vaultwarden
--- modules/system/user.nix
+++ modules/system/user.nix
@@ -10,11 +10,43 @@
           description = username;
           name = username;
           shell = pkgs.zsh;
+          uid = 1000;
           ignoreShellProgramCheck = true;
           extraGroups = ["networkmanager" "samba" "wheel" "media" "dialout"];
           isNormalUser = true;
         };
-        groups.media = {};
+        groups.media.gid = 991;
+      };
+    };
+    nixos.homelab = {
+      config,
+      lib,
+      ...
+    }: {
+      options.homelab = {
+        user = lib.mkOption {
+          default = "media";
+          type = lib.types.str;
+          description = "User to run the homelab services as";
+        };
+        group = lib.mkOption {
+          default = "media";
+          type = lib.types.str;
+          description = "Group to run homelab as";
+        };
+      };
+      config = let
+        inherit (config.homelab) group user;
+      in {
+        users = {
+          users.${user} = {
+            isSystemUser = true;
+            inherit group;
+            uid = 992;
+            home = "/var/lib/media";
+          };
+          groups.${group}.gid = 991;
+        };
       };
     };
     darwin.user = {
