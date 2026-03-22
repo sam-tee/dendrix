f9471d40d77cd5cac1ba0c74c5e6df03d2897ae2
sam-tee <93236986+sam-tee@users.noreply.github.com>
2026-03-22 21:55:59 +0000

    fix: hyprgrass

--- modules/de/hyprland/_hyprgrass.nix
+++ modules/de/hyprland/_hyprgrass.nix
@@ -2,26 +2,34 @@
   lib,
   fetchFromGitHub,
   cmake,
+  doctest,
   ninja,
   meson,
   wf-touch,
+  nix-update-script,
   hyprland,
   hyprlandPlugins,
 }:
-hyprlandPlugins.mkHyprlandPlugin (finalAttrs: {
+hyprlandPlugins.mkHyprlandPlugin {
   pluginName = "hyprgrass";
   version = "0.54.2";
 
   src = fetchFromGitHub {
     owner = "horriblename";
     repo = "hyprgrass";
-    rev = "hl-${finalAttrs.version}";
+    rev = "hl-0.54.2";
     hash = "sha256-XI9j4gXRMeMR+dycRMZ1QwbVK5xYoXDBbihFeGapv04=";
   };
 
-  nativeBuildInputs = [cmake ninja meson];
+  nativeBuildInputs = [cmake ninja meson doctest];
 
   buildInputs = [wf-touch];
+
+  dontUseCmakeConfigure = true;
+
+  doCheck = true;
+
+  passthru.updateScript = nix-update-script {};
 
   meta = with lib; {
     homepage = "https://github.com/horriblename/hyprgrass";
@@ -29,4 +37,4 @@ hyprlandPlugins.mkHyprlandPlugin (finalAttrs: {
     license = licenses.bsd3;
     platforms = platforms.linux;
   };
-})
+}
