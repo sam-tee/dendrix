4806a70498fec35432a050c18d626cab027399a1
sam-tee <93236986+sam-tee@users.noreply.github.com>
2026-03-23 17:41:45 +0000

    feat: added screen setup for duet3

--- modules/hosts/_hardware/duet3.nix
+++ modules/hosts/_hardware/duet3.nix
@@ -11,6 +11,18 @@
     }
   ];
   home-manager.users.${username} = {
-    hypr.monitors = ["DSI-1,2000x1200@60,auto,1,transform,1"];
+    hypr.monitors = ["DSI-1,1200x2000@60,0x0,1,transform,1"];
+    wayland.windowManager.hyprland.settings.device = [
+      {
+        name = "hid-over-i2c-0603:604a";
+        output = "DSI-1";
+        trnsform = 1;
+      }
+      {
+        name = "hid-over-i2c-0603:604a-stylus";
+        output = "DSI-1";
+        transform = 1;
+      }
+    ];
   };
 }
