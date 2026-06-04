d10a97bf2330a9c0edbea3510eb1e92d987508c0
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-06-04 04:44:53 +0100

    feat: updated deploy script

--- bin/deploy.py
+++ bin/deploy.py
@@ -3,13 +3,71 @@ import argparse
 import os
 import subprocess
 import sys
-import tempfile
 from pathlib import Path
+
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import PlainScalarString
+
+
+def update_secrets(
+    host_age_key: str,
+    target_host: str,
+    secret_dir: Path,
+    flake_path: Path,
+):
+    yaml_file = secret_dir / ".sops.yaml"
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    yaml.indent(mapping=2, sequence=4, offset=2)
+    data = yaml.load(yaml_file.read_text())
+    keys = data["keys"]
+
+    found = False
+    for idx, key in enumerate(keys):
+        anchor = getattr(key, "anchor", None)
+        anchor_name = anchor.value if anchor else None
+        if anchor_name == target_host:
+            replacement = PlainScalarString(host_age_key)
+            replacement.yaml_set_anchor(target_host, always_dump=True)
+            keys[idx] = replacement
+            found = True
+            break
+    if not found:
+        sys.exit(1)
+    with yaml_file.open("w") as f:
+        yaml.dump(data, f)
+
+    subprocess.run(["nix", "flake", "update", "secrets"], cwd=flake_path)
+
+    subprocess.run(
+        ["git", "commit", "-am", f"deploy: updated host {target_host}"], cwd=secret_dir
+    )
+    subprocess.run(["git", "push"], cwd=secret_dir)
+
+
+def get_age_key(host_ip: str, host_port: int) -> str:
+    keyscan_cmd = [
+        "ssh-keyscan",
+        "-t",
+        "ed25519",
+        "-P",
+        str(host_port),
+        host_ip,
+    ]
+    keyscan = subprocess.run(keyscan_cmd, text=True, check=False)
+
+    ssh_to_age = subprocess.run(
+        ["ssh-to-age"], input=keyscan.stdout, text=True, check=False
+    )
+    keys = [
+        line.strip()
+        for line in ssh_to_age.stdout.splitlines()
+        if line.strip().startswith("age1")
+    ]
+    return keys[0]
 
 
 def deploy(
-    local_priv_key: Path,
-    local_pub_key: Path,
     flake_uri: str,
     ssh_host: str,
     build_remote: bool,
@@ -17,47 +75,30 @@ def deploy(
     """
     Runs nixos-anywhere with specified parameters
     """
-    with tempfile.TemporaryDirectory(prefix="deploy-secrets-") as tmpdir:
-        tmp_path = Path(tmpdir)
-        ssh_dir = tmp_path / "etc" / "ssh"
-        ssh_dir.mkdir(parents=True, exist_ok=True)
-        priv_key = ssh_dir / "ssh_host_ed25519_key"
-        pub_key = ssh_dir / "ssh_host_ed25519_key.pub"
-        priv_key.write_bytes(local_priv_key.read_bytes())
-        pub_key.write_bytes(local_pub_key.read_bytes())
+    cmd = [
+        "sudo",
+        "nix",
+        "run",
+        "github:nix-community/nixos-anywhere",
+        "--",
+        "--flake",
+        f"{flake_uri}",
+        "--copy-host-keys",
+        "--target-host",
+        ssh_host,
+    ]
+    if build_remote:
+        cmd.extend("--build-on-remote")
 
-        os.chmod(tmp_path / "etc", 0o755)
-        os.chmod(ssh_dir, 0o755)
-        os.chmod(priv_key, 0o600)
-        os.chmod(pub_key, 0o644)
+    print(f"Running: {' '.join(cmd)}\n")
 
-        cmd = [
-            "nix",
-            "run",
-            "github:nix-community/nixos-anywhere",
-            "--",
-            "--flake",
-            f"{flake_uri}",
-            "--extra-files",
-            str(tmp_path),
-            "--target-host",
-            ssh_host,
-        ]
-        if build_remote:
-            cmd.extend("--build-on-remote")
-
-        print(f"Running: {' '.join(cmd)}\n")
-
-        try:
-            subprocess.run(cmd, check=True)
-        except subprocess.CalledProcessError as e:
-            print(e)
+    try:
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
 
 
 if __name__ == "__main__":
-    if os.geteuid() != 0:
-        print("Script must be run as root")
-        sys.exit(1)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--host",
@@ -70,35 +111,35 @@ if __name__ == "__main__":
         "--flake",
         "-F",
         type=Path,
-        help="Path to flake. Defaults to root of this repo.",
-        default=Path(__file__).parent.parent.resolve(),
+        help="Path to flake. Defaults to NH_FLAKE",
+        default=os.getenv("NH_FLAKE"),
     )
     parser.add_argument(
-        "--pub",
+        "--secrets",
+        "-S",
         type=Path,
-        help="Path to pubkey for new host. Defaults to /home/sam/.ssh/keys/{host}.pub",
-    )
-    parser.add_argument(
-        "--priv",
-        type=Path,
-        help="Path to privkey for new host. Defaults to /home/sam/.ssh/keys/{host}",
+        help="Path to secrets directory",
+        default=Path("~/nix-secrets").resolve(),
     )
     parser.add_argument(
         "--target",
         "-T",
         type=str,
-        help="Target SSH hostname - usually nixos@<ip>",
+        help="Target SSH host in form <user>@<ip/hostname>",
         required=True,
     )
+    parser.add_argument("--port", "-p", type=int, help="Port to access ssh", default=22)
     parser.add_argument("--remote", "-R", action="store_true", help="build on remote")
     args = parser.parse_args()
-    if args.priv is None:
-        args.priv = Path(f"/home/sam/.ssh/keys/{args.host}")
-    if args.pub is None:
-        args.pub = Path(f"/home/sam/.ssh/keys/{args.host}.pub")
+    ip, user = args.target.split("@")
+    age_key = get_age_key(host_ip=ip, host_port=args.port)
+    update_secrets(
+        host_age_key=age_key,
+        target_host=args.host,
+        secret_dir=args.secrets,
+        flake_path=args.flake,
+    )
     deploy(
-        local_priv_key=args.priv,
-        local_pub_key=args.pub,
         flake_uri=f"{args.flake}#{args.host}",
         ssh_host=args.target,
         build_remote=args.remote,
