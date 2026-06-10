87d96659c694a22dd3ec6c89fde2a179afc73016
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-06-11 00:01:34 +0100

    feat: added stirling-pdf

---
+++ modules/homelab/stirling-pdf.nix
@@ -0,0 +1,32 @@
+{
+  flake.modules.nixos.pdf = {config, ...}: let
+    inherit (config.homelab) dataDir email;
+  in {
+    sops.secrets.stirling = {};
+    services.stirling-pdf = {
+      enable = true;
+      environment = {
+        SECURITY_ENABLELOGIN = true;
+        LANGS = "en_GB";
+        SYSTEM_DEFAULTLOCALE = "en-GB";
+        MAIL_ENABLED = true;
+        MAIL_FROM = email.from;
+        MAIL_HOST = email.host;
+        MAIL_PORT = 587;
+        MAIL_USERNAME = email.user;
+        MAIL_TLS_ENABLED = true;
+        UI_LOGOSTYLE = "modern";
+        INSTALL_BOOK_AND_ADVANCED_HTML_OPS = true;
+        SERVER_PORT = 8080;
+        STORAGE_ENABLED = true;
+        STORAGE_PROVIDER = "local";
+        STORAGE_LOCAL_BASEPATH = "${dataDir}/stirling";
+        STORAGE_SHARING_ENABLED = true;
+        STORAGE_SHARING_LINKENABLED = true;
+        STORAGE_SHARING_EMAILENABLED = true;
+        STORAGE_SHARING_LINKEXPIRATIONDAYS = 3;
+      };
+      environmentFiles = config.sops.secrets.stirling.path;
+    };
+  };
+}
