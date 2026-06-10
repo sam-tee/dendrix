41b0c457f364afed2f2712a368d8f2b4463b8126
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-06-11 00:04:45 +0100

    feat: changed python dev setup

--- modules/cli/python.nix
+++ modules/cli/python.nix
@@ -1,21 +1,10 @@
-let
-  pyPkgs = pkgs:
-    with pkgs; [
+{
+  flake.modules.homeManager.cli = {pkgs, ...}: {
+    home.packages = with pkgs; [
       python3
       ruff
       ty
       uv
     ];
-in {
-  flake.modules = {
-    nixos.cli = {pkgs, ...}: {
-      environment.systemPackages = pyPkgs pkgs;
-    };
-    darwin.cli = {pkgs, ...}: {
-      environment.systemPackages = pyPkgs pkgs;
-    };
-    homeManager.cli = {pkgs, ...}: {
-      home.packages = pyPkgs pkgs;
-    };
   };
 }
