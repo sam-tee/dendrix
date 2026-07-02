1b60a4475c728a46623f9a90dcdc63ed9c62a349
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-07-02 17:08:54 +0100

    feat: added ugly overlay for mobile compat

--- modules/bundles.nix
+++ modules/bundles.nix
@@ -1,4 +1,9 @@
-{self, ...}: let
+{
+  inputs,
+  lib,
+  self,
+  ...
+}: let
   dMod = self.modules.darwin;
   hMod = self.modules.homeManager;
   nMod = self.modules.nixos;
@@ -23,6 +28,46 @@ in {
       _mobile.imports = with nMod; [
         _minimal
         services
+        ({config, ...}: {
+          hardware.deviceTree.enable = lib.mkDefault (config.boot.kernelPackages.kernel.buildDTBs or false);
+          nixpkgs.overlays = lib.mkAfter [
+            (final: prev: let
+              linuxKernelCompat = platform: {
+                target =
+                  if platform.isx86
+                  then "bzImage"
+                  else if platform.isAarch32
+                  then "zImage"
+                  else if platform.isAarch64 || platform.isRiscV
+                  then "Image"
+                  else if platform.isLoongArch64
+                  then "vmlinuz.efi"
+                  else "vmlinux";
+                DTB = platform.isAarch || platform.isRiscV || platform.isLoongArch64;
+              };
+              withLinuxKernelCompat = stdenv:
+                stdenv
+                // {
+                  hostPlatform =
+                    stdenv.hostPlatform
+                    // {
+                      linux-kernel = linuxKernelCompat stdenv.hostPlatform;
+                    };
+                };
+            in {
+              mobile-nixos =
+                prev.mobile-nixos
+                // {
+                  kernel-builder = final.callPackage "${inputs.mobile-nixos}/overlay/mobile-nixos/kernel/builder.nix" {
+                    stdenv = withLinuxKernelCompat final.stdenv;
+                  };
+                  kernel-builder-clang = final.callPackage "${inputs.mobile-nixos}/overlay/mobile-nixos/kernel/builder.nix" {
+                    stdenv = with final; withLinuxKernelCompat (overrideCC stdenv buildPackages.clang);
+                  };
+                };
+            })
+          ];
+        })
       ];
       _serverMin.imports = with nMod; [
         _minimal
