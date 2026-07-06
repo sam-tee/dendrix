fdf3c55f2e4c6224c253815db115a33d9d4bd92b
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-07-06 23:00:41 +0100

    feat: updated flake

--- flake.nix
+++ flake.nix
@@ -39,7 +39,6 @@
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     nixgl.url = "github:nix-community/nixgl";
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixvim.url = "github:nix-community/nixvim";
     noctalia.url = "github:noctalia-dev/noctalia";
     paneru = {
