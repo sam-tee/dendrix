4ecda43014933b197ff04a0a0357048469a2ee07
Sam Tee <93236986+sam-tee@users.noreply.github.com>
2026-07-06 23:04:01 +0100

    feat: added atticd substituter

--- modules/nix.nix
+++ modules/nix.nix
@@ -74,6 +74,8 @@ in {
           keep-going = true;
           warn-dirty = false;
           builders-use-substitutes = true;
+          substituters = lib.mkBefore ["https://cache.akhlus.uk/dendrix"];
+          trusted-public-keys = lib.mkBefore ["dendrix:gw3GtUeu7QiYchM+GKrwanxDeUqa/Ddl45l8x05rD+o="];
           flake-registry = "";
           experimental-features = [
             "flakes"
