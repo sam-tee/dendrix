c887ec9fefebd04b7ed3b35b4d2efa8145bd449b
Sam Tee <sam.tee4@proton.me>
2026-07-15 03:19:18 +0100

    feat: updated attic setup

--- modules/homelab/atticd.nix
+++ modules/homelab/atticd.nix
@@ -4,21 +4,27 @@
     pkgs,
     ...
   }: let
-    inherit (config.homelab) domain;
+    inherit (config.homelab) dataDir domain group user;
     inherit (self.services.atticd) port subdomain;
+    atticDir = "${dataDir}/attic";
   in {
     sops.secrets."atticd-env" = {};
 
     services.atticd = {
       enable = true;
+      inherit group user;
       environmentFile = config.sops.secrets."atticd-env".path;
       settings = {
         listen = "0.0.0.0:${toString port}";
         api-endpoint = "https://${subdomain}.${domain}/";
+        storage = {
+          type = "local";
+          path = "${atticDir}/storage";
+        };
+        database.url = "sqlite://${atticDir}/server.db?mode=rwc";
       };
     };
-
+    systemd.tmpfiles.rules = ["d ${atticDir} 0775 ${user} ${group} -"];
     environment.systemPackages = [pkgs.attic-client];
-    networking.firewall.allowedTCPPorts = [port];
   };
 }
