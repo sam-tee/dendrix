13ab798daab8418f110ee5423bbd6c37a6fa5529
Sam Tee <sam.tee4@proton.me>
2026-08-13 01:14:30 +0100

    feat: finished refactoring and porting gui package config

--- modules/gui/ghostty.nix
+++ modules/gui/ghostty.nix
@@ -2,20 +2,54 @@
   inherit (self.cosmetic) fonts theme;
   inherit (theme) attrs defaultVariant;
 in {
-  flake.modules.homeManager.ghostty = {
+  flake.modules.hjem.ghostty = {
     lib,
     pkgs,
     ...
   }: let
     isLinux = pkgs.stdenv.hostPlatform.isLinux;
   in {
-    programs.ghostty = {
-      enable = true;
-      package =
-        if isLinux
-        then pkgs.ghostty
-        else pkgs.ghostty-bin;
-      settings = {
+    files = let
+      inherit (lib.generators) mkKeyValueDefault toKeyValue;
+      mkConfig = values: {
+        generator = toKeyValue {
+          mkKeyValue = mkKeyValueDefault {} " = ";
+          listsAsDuplicateKeys = true;
+        };
+        value = values;
+      };
+      mkTheme = variant: let
+        rmHash = lib.removePrefix "#";
+      in
+        with attrs.${variant}; {
+          background-blur = 20;
+          background-opacity = (lib.fromHexString attrs.opacity) * 100 / 255 / 100.0;
+          background = rmHash base00;
+          foreground = rmHash base05;
+          cursor-color = rmHash base05;
+          selection-background = rmHash base02;
+          selection-foreground = rmHash base05;
+          palette = [
+            "0=${base00}"
+            "1=${base08}"
+            "2=${base0B}"
+            "3=${base0A}"
+            "4=${base0D}"
+            "5=${base0E}"
+            "6=${base0C}"
+            "7=${base05}"
+            "8=${base03}"
+            "9=${base08}"
+            "10=${base0B}"
+            "11=${base0A}"
+            "12=${base0D}"
+            "13=${base0E}"
+            "14=${base0C}"
+            "15=${base07}"
+          ];
+        };
+    in {
+      ".config/ghostty/config" = mkConfig {
         font-family = [fonts.mono.name];
         font-size =
           if isLinux
@@ -29,41 +63,8 @@ in {
         ];
         copy-on-select = "clipboard";
       };
-      themes = let
-        mkTheme = variant: let
-          rmHash = lib.removePrefix "#";
-        in
-          with attrs.${variant}; {
-            background-blur = 20;
-            background-opacity = (lib.fromHexString attrs.opacity) * 100 / 255 / 100.0;
-            background = rmHash base00;
-            foreground = rmHash base05;
-            cursor-color = rmHash base05;
-            selection-background = rmHash base02;
-            selection-foreground = rmHash base05;
-            palette = [
-              "0=${base00}"
-              "1=${base08}"
-              "2=${base0B}"
-              "3=${base0A}"
-              "4=${base0D}"
-              "5=${base0E}"
-              "6=${base0C}"
-              "7=${base05}"
-              "8=${base03}"
-              "9=${base08}"
-              "10=${base0B}"
-              "11=${base0A}"
-              "12=${base0D}"
-              "13=${base0E}"
-              "14=${base0C}"
-              "15=${base07}"
-            ];
-          };
-      in {
-        "${attrs.name}-dark" = mkTheme "dark";
-        "${attrs.name}-light" = mkTheme "light";
-      };
+      ".config/ghostty/themes/${attrs.name}-dark" = mkConfig (mkTheme "dark");
+      ".config/ghostty/themes/${attrs.name}-light" = mkConfig (mkTheme "light");
     };
   };
 }
---
+++ modules/gui/guiPackages.nix
@@ -0,0 +1,60 @@
+{self, ...}: {
+  flake.modules = {
+    nixos = {
+      gui = self.modules.nixos.linuxBase;
+      linuxBase = {pkgs, ...}: {
+        environment = {
+          variables.SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
+          systemPackages = with pkgs;
+            [
+              bitwarden
+              ghostty
+              proton-vpn
+              xournalpp
+              zed-editor
+            ]
+            ++ lib.optionals (pkgs.stdenv.hostPlatform.system != "aarch64-linux") (with pkgs; [
+              discord
+              spotify
+            ]);
+        };
+        programs.localsend.enable = true;
+      };
+      linuxAll = {pkgs, ...}: {
+        imports = [self.modules.nixos.linuxBase];
+        environment.systemPackages = with pkgs; [
+          anki
+          baobab
+          google-chrome
+          gnome-disk-utility
+          haruna
+          libreoffice
+        ];
+        programs.vscode.enable = true;
+      };
+    };
+    darwin = {
+      default = self.modules.darwin.macosBase;
+      macosBase = {pkgs, ...}: {
+        hjem.extraModules = with self.modules.hjem; [ghostty zed];
+        environment.systemPackages = with pkgs; [
+          discord
+          ghostty-bin
+          iina
+          skimpdf
+          spotify
+          zed-editor
+        ];
+        homebrew = {
+          casks = [
+            "google-chrome"
+            "localsend"
+            "raycast"
+            "whatsapp"
+          ];
+          masApps = {"Bitwarden" = 1352778147;};
+        };
+      };
+    };
+  };
+}
