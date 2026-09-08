2574003479c8378b6d9f95597b40fc2be7ac2739
Sam Tee <sam.tee4@proton.me>
2026-09-08 17:33:26 +0100

    refactor: ai module

--- modules/cli/ai.nix
+++ modules/cli/ai.nix
@@ -1,8 +1,11 @@
 {
+  lib,
   moduleWithSystem,
   self,
   ...
-}: {
+}: let
+  inherit (lib) singleton;
+in {
   flake-file.inputs.ai = {
     url = "github:numtide/llm-agents.nix";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -11,10 +14,11 @@
     default = self.modules.generic.ai;
     ai = moduleWithSystem ({inputs', ...}: _: {
       nix.settings = {
-        extra-substituters = ["https://cache.numtide.com"];
-        extra-trusted-public-keys = ["niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="];
+        extra-substituters = singleton "https://cache.numtide.com";
+        extra-trusted-public-keys = singleton "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=";
       };
       environment.systemPackages = with inputs'.ai.packages; [opencode];
+      hjem.extraModules = singleton {environment.sessionVariables.T3CODE_BASE_DIR = "$XDG_CONFIG_HOME/t3";};
     });
   };
 }
