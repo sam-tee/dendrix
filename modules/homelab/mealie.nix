fd091dc76fa87c03b3a52aa56f924671c3ad1b97
Sam Tee <sam.tee4@proton.me>
2026-09-10 16:31:02 +0100

    fix: added home dir for mealie

--- modules/homelab/mealie.nix
+++ modules/homelab/mealie.nix
@@ -1,10 +1,12 @@
 {self, ...}: {
   flake.modules.nixos.mealie = {config, ...}: let
-    inherit (config.homelab.email) host user from;
+    inherit (config.homelab) dataDir email;
+    inherit (email) host user from;
     inherit (self.services.mealie) port subdomain;
     mkIP = host: self.hosts.${host}.tailscaleIP;
   in {
     sops.secrets."mealieEnv" = {};
+    systemd.services.mealie.environment.HOME = "${dataDir}/mealie";
     services.mealie = {
       enable = true;
       inherit port;
