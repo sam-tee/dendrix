65dd4dcd4b5e264cf131a85c160c6135793561bb
Sam Tee <sam.tee4@proton.me>
2026-09-10 02:35:55 +0100

    feat: added prefs to helium setup

--- modules/gui/helium.nix
+++ modules/gui/helium.nix
@@ -9,9 +9,66 @@
   };
   flake.modules = {
     darwin.default = self.modules.generic.helium;
-    nixos.gui = self.modules.generic.helium;
+    nixos.gui = self.modules.nixos.helium;
     generic.helium = moduleWithSystem ({inputs', ...}: _: {
       environment.systemPackages = [inputs'.helium.packages.default];
+      hjem.extraModules = [self.modules.hjem.helium];
     });
+    nixos.helium = {
+      imports = [self.modules.generic.helium];
+      programs.chromium = {
+        enable = true;
+        extensions = [
+          "eimadpbcbfnmbkopoojfekhnkhdbieeh"
+          "ekhagklcjbdpajgpjgmbionohlpdbjgc"
+          "jplgfhpmjnbigmhklmmbgecoobifkmpa"
+          "mbhadeogepkjdjeikcckdkjdjhhkhlid"
+          "mnjggcdmjocbbbhaepdhchncahnbgone"
+          "nffaoalbilbmmfgbnbgppjihopabppdk"
+          "nngceckbapebfimnlniiiahkandclblb"
+          "pkehgijcmpdhfbdbbnkijodmdjhbjlgp"
+          "pnidmkljnhbjfffciajlcpeldoljnidn"
+        ];
+      };
+    };
+    hjem.helium = {
+      lib,
+      pkgs,
+      ...
+    }: let
+      isLinux = pkgs.stdenv.hostPlatform.isLinux;
+      preferences = {
+        helium = {
+          completed_onboarding = true;
+          services.user_consented = true;
+          browser = {
+            centered_location_bar = true;
+            layout = 2;
+            minimal_location_bar = false;
+            rounded_frame = false;
+            vertical_right_aligned = true;
+            zen_mode = false;
+            zen_mode_sidebar_pinned = true;
+            zen_mode_top_chrome_pinned = true;
+          };
+        };
+        browser.theme.is_grayscale2 = true;
+        intl.selected_languages = "en-GB,en-US,en";
+        spellcheck.dictionaries = ["en-GB"];
+        vertical_tabs = {
+          collapsed_state = true;
+          uncollapsed_width = 200;
+        };
+        privacy_sandbox.first_party_sets_enabled = false;
+      };
+      preferenceFile = {
+        generator = lib.generators.toJSON {};
+        value = preferences;
+        type = "copy";
+      };
+    in {
+      xdg.config.files."net.imput.helium/Default/Preferences" = lib.mkIf isLinux preferenceFile;
+      files."Library/Application Support/net.imput.helium/Default/Preferences" = lib.mkIf (!isLinux) preferenceFile;
+    };
   };
 }
