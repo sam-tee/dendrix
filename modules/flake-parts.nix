1f4f6994d346fe211101f76cd3de712211901ec5
Sam Tee <sam.tee4@proton.me>
2026-09-24 02:37:59 +0100

    chore(flake): configure Alejandra formatter

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -1,5 +1,6 @@
 {inputs, ...}: {
   flake-file.inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+  flake-file.formatter = pkgs: pkgs.alejandra;
   systems = [
     "x86_64-linux"
     "aarch64-linux"
