47bff08be95dbed56c13fd9422135d1136774123
Sam Tee <sam.tee4@proton.me>
2026-09-26 19:54:18 +0100

    feat(kernel): update kernel params

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -21,6 +21,7 @@
         "rd.systemd.show_status=false"
         "rd.udev.log_level=3"
         "udev.log_priority=3"
+        "plymouth.use-simpledrm"
       ];
       loader = {
         systemd-boot = {
